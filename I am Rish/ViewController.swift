74f24596f4555d7f5156f469cc65362a32290fab
Raphael Martins <raphael.almeida.martins@gmail.com>
2023-04-25 01:16:33 -0300

    Initial Commit

---
+++ I am Rish/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rish
+//
+//  Created by Raphael Martins on 25/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
